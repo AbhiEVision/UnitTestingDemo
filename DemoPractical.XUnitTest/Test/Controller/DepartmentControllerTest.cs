d51097bb6bc6312b816813918140ae2bc642442d
Abhi Patel <abhi.d@simformsolutions.com>
2023-08-23 17:01:56 +0530

    add : xUnit test demo with FakeItEasy library and Moq

---
+++ DemoPractical.XUnitTest/Test/Controller/DepartmentControllerTest.cs
@@ -0,0 +1,97 @@
+﻿using DemoPractical.API.Controllers.V2;
+using DemoPractical.Domain.Interface;
+using DemoPractical.Models.Models;
+using FakeItEasy;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace DemoPractical.XUnitTest.Test.NewFolder
+{
+	public class DepartmentControllerTest
+	{
+		private readonly DepartmentController _departmentController;
+		private readonly IDepartmentRepository _repository;
+
+		public DepartmentControllerTest()
+		{
+			// Dependencies
+			_repository = A.Fake<IDepartmentRepository>();
+
+			//SUT
+			_departmentController = new DepartmentController(_repository);
+
+		}
+
+		[Fact]
+		public async Task DepartmentController_GetDepartment_ReturnOkObjectResult_FakeItEasy()
+		{
+			// Arrange
+			int departmentId = 1;
+			A.CallTo(() => _repository.GetDepartmentByIdAsync(departmentId)).Returns(new Department());
+
+			// Act
+			var result = await _departmentController.GetDepartment(departmentId);
+
+			// Assertion
+			result.Should().NotBeNull();
+			result.Should().BeOfType(typeof(OkObjectResult));
+		}
+
+		[Fact]
+		public async Task DepartmentController_CreateDepartment_ReturnOkObjectResult_FakeItEasy()
+		{
+			// Arrange
+			var department = A.Fake<Department>();
+			var departmentController = new DepartmentController(_repository);
+			A.CallTo(() => _repository.AddDepartment(department));
+
+
+			// Act
+			var result = await departmentController.CreateDepartment(department);
+
+			//Assert
+			result.Should().NotBeNull();
+			result.Should().BeOfType(typeof(OkObjectResult));
+
+		}
+
+		[Fact]
+		public async Task DepartmentController_CreateDepartment_ReturnOkObjectResult_Mock()
+		{
+			// Arrange
+			var department = new Mock<Department>();
+			var departmentRepository = new Mock<IDepartmentRepository>();
+			departmentRepository.Setup(x => x.AddDepartment(department.Object));
+			var departmentController = new DepartmentController(departmentRepository.Object);
+
+			// Act
+			var result = await departmentController.CreateDepartment(department.Object);
+
+			// Assert
+			result.Should().NotBeNull();
+			result.Should().BeOfType(typeof(OkObjectResult));
+
+
+		}
+
+		[Fact]
+		public async Task DepartmentController_GetDepartment_ReturnOkObjectResult_Mock()
+		{
+			// Arrange
+			var departmentId = 1;
+			var departmentRepository = new Mock<IDepartmentRepository>();
+
+			departmentRepository.Setup(x => x.GetDepartmentByIdAsync(departmentId)).Returns(Task.FromResult(new Department()));
+			var departmentController = new DepartmentController(departmentRepository.Object);
+
+			// Act
+			var result = await departmentController.GetDepartment(departmentId);
+
+			// Assertion
+			result.Should().NotBeNull();
+			result.Should().BeOfType(typeof(OkObjectResult));
+		}
+
+	}
+}
---
+++ DemoPractical.XUnitTest/Test/Repositories/DepartmentRepositoryTest.cs
@@ -0,0 +1,103 @@
+﻿using DemoPractical.DataAccessLayer.Data;
+using DemoPractical.DataAccessLayer.Repositories;
+using DemoPractical.Models.Models;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoPractical.XUnitTest.Test.Repositories
+{
+	public class DepartmentRepositoryTest
+	{
+		public async Task<ApplicationDataContext> GetDataContext()
+		{
+			var options = new DbContextOptionsBuilder<ApplicationDataContext>()
+				.UseInMemoryDatabase("TempDatabase")
+				.Options;
+
+			var databaseContext = new ApplicationDataContext(options);
+
+			await databaseContext.Database.EnsureCreatedAsync();
+
+			if (await databaseContext.Departments.CountAsync() <= 0)
+			{
+				databaseContext.Departments.AddRange(new Department()
+				{
+					Id = 1,
+					DepartmentName = "HR"
+				},
+				new Department()
+				{
+					Id = 2,
+					DepartmentName = "JAVA"
+				},
+				new Department()
+				{
+					Id = 3,
+					DepartmentName = "DOTNET"
+				},
+				new Department()
+				{
+					Id = 4,
+					DepartmentName = "IT"
+				});
+			}
+
+			await databaseContext.SaveChangesAsync();
+
+			return databaseContext;
+
+		}
+
+		[Fact]
+		public async Task DepartmentRepository_GetDepartmentByIdAsync_ReturnsDepartment()
+		{
+			// Arrange
+			var databaseContext = await GetDataContext();
+			var repository = new DepartmentRepository(databaseContext);
+			int departmentId = 1;
+			string expectedDepatmentName = "HR";
+
+			// Act
+			var result = await repository.GetDepartmentByIdAsync(departmentId);
+
+
+			// Assert
+			result.Should().NotBeNull();
+			result.Should().BeOfType<Department>();
+			result.DepartmentName.Should().Be(expectedDepatmentName);
+		}
+
+		[Fact]
+		public async Task DepartmentRepository_IsDepartmentExists_ReturnsTrue()
+		{
+			// Arrange
+			var databaseContext = await GetDataContext();
+			var repository = new DepartmentRepository(databaseContext);
+			int departmentId = 1;
+
+			// Act
+			var result = await repository.IsDepartmentExists(departmentId);
+
+
+			// Assert
+			result.Should().Be(true);
+		}
+
+		[Fact]
+		public async Task DepartmentRepository_IsDepartmentExists_ReturnsFalse()
+		{
+			// Arrange
+			var databaseContext = await GetDataContext();
+			var repository = new DepartmentRepository(databaseContext);
+			int departmentId = 10;
+
+			// Act
+			var result = await repository.IsDepartmentExists(departmentId);
+
+
+			// Assert
+			result.Should().Be(false);
+		}
+
+	}
+}
